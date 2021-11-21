678fe57a644721130851f512c78f0ed0a6c6a44d
Marc Malliate <marc.malliate@gmail.com>
2021-11-22 10:19:04 +1030

    uuid added

--- server.js
+++ server.js
@@ -1,19 +1,33 @@
-// Express and routes required
+// Import express and routes required
 const express = require('express');
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const path = require('path');
+const api = require('./routes/index');
 
 // Initialise the app and create a port
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.use('/api', apiRoutes);
+app.use('./api', api);
 app.use('/', htmlRoutes);
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, './public/index.html'))
+);
+
+//GET route for notes
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+//GET wildcard route to redirect back to the  homepage 
+app.get('*', (req, res) => res.redirect('/'));
+
 
 // Start the server on the port
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
