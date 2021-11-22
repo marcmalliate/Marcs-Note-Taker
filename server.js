802d0ae6c54d322b8704111ae62378be574e509b
Marc Malliate <marc.malliate@gmail.com>
2021-11-22 13:27:56 +1030

    server.js edited

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.use('./api', api);
-app.use('/', htmlRoutes);
+
 
 // GET Route for homepage
 app.get('/', (req, res) =>
